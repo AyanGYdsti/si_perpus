ef9c1b5b4c5f06ba63923f5efae6534239f16e3b
MuhammadSafril <muhammadsafril@gmail.com>
2024-10-05 01:20:56 -0700

    membuat halaman beranda

---
+++ app/Http/Controllers/Frontend/BerandaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BerandaController extends Controller
+{
+    public function index()
+    {
+        $title = "Beranda";
+
+        return view('frontend.beranda.index', compact('title'));
+    }
+}
---
+++ resources/views/frontend/beranda/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main_frontend')
+
+@section('content')
+    <div style="height: 80vh;width:100%;background-image:url('assets/img/gambar-5.png');background-size:cover"
+        class="d-flex justify-content-center align-items-center text-white">
+        <div class="d-flex flex-column justify-content-center align-items-center bg-dark p-4"
+            style="width: 90%;--bs-bg-opacity: .5;border-radius:10px">
+            <h1>SELAMAT DATANG DI PERPUSTAKAAN ONLINE FMIPA UHO</h1>
+            <a href="/buku" class="btn btn-primary">JELAJAHI BUKU</a>
+        </div>
+    </div>
+    </div>
+@endsection
