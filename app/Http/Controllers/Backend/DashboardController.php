7ed3a806b2f23856f1e2032ae15659a7eba43c43
AnggunPratiwi <anggunpratiwiii081@gmail.com>
2024-10-13 12:05:52 +0800

    menambahkan jumlah anggota dan jumlah buku di halaman dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Models\Anggota;
+use App\Models\Buku;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,6 +14,10 @@ class DashboardController extends Controller
     {
         $title = "Dasboard";
 
-        return view('backend.dashboard.index', compact('title'));
+        $jmlAnggota=Anggota::count();
+        $jmlBuku=Buku::count();
+
+
+        return view('backend.dashboard.index', compact('title','jmlAnggota','jmlBuku'));
     }
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -11,7 +11,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Jumlah Anggota</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$jmlAnggota}}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -28,7 +28,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Jumlah Buku</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">100</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$jmlBuku}}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
